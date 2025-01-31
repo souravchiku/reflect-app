247bc1d012899d1f50db3ef68f25417473df8457
Sourav kumar <souravchiku0@gmail.com>
2025-01-31 18:18:39 +0530

    installed all the component from shadui

--- reflect-app/app/page.js
+++ reflect-app/app/page.js
@@ -1,9 +1,12 @@
+import { Button } from "@/components/ui/button"
+
+
 
 export default function Home() {
-  const home = 1
+  
   return (
    <div>
-    hello chiku
+    <Button variant="journal">Button</Button>
    </div>
   );
 }
