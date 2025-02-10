f0e4e14e9bd4ca53e3e9427884949c7604b53ba8
Sourav kumar <souravchiku0@gmail.com>
2025-02-10 17:37:26 +0530

    added collections folder after logged in

--- reflect-app/components/header.jsx
+++ reflect-app/components/header.jsx
@@ -2,11 +2,13 @@ import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import { Button } from './ui/button'
+import { FolderOpen, PenBox } from 'lucide-react'
 
 const Header = () => {
     return (
-        <header className='container mx-auto flex justify-between'>
-            <nav className='py-6 px-4 flex justify-between itens-center ml-10'>
+        <header className='container mx-auto '>
+            <nav className='py-6 px-4 flex justify-between items-center'>
                 <Link href="/">
                     <Image
                         src="/logo.png"
@@ -17,16 +19,26 @@ const Header = () => {
                     />
                 </Link>
                 <div className='flex items-center gap-4'>
+                <SignedIn>
+                <Link href={'/dashboard#collections'}>
+                    <Button variant="outline" className="flex items-center gap-2"> <FolderOpen size={18}/> <span className="hidden md:inline">Collections</span></Button>
+                </Link>
+                </SignedIn>
 
+                <Link href={'/journal/write'}>
+                    <Button variant="journal" className="flex items-center gap-2"> <PenBox size={18}/> <span className="hidden md:inline">Write New</span></Button>
+                </Link>
+
+                <SignedOut>
+                    <SignInButton forceRedirectUrl='/dashboard'>
+                        <Button variant="outline" >Login</Button>
+                    </SignInButton >
+                </SignedOut>
                 </div>
             </nav>
             <div className='flex item-center mr-10'>
-                <SignedOut>
-                    <SignInButton />
-                </SignedOut>
-                <SignedIn>
-                    <UserButton />
-                </SignedIn>
+               
+            
             </div>
         </header>
 
